fbcaaede94881648ec66faf5666700af3ebccc6c
sushantkdm <109025450+sushantkdm@users.noreply.github.com>
2023-03-29 19:22:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	const phone={
+        '0':"0",
+        '1':'1',
+        '2':'abc',
+        '3':'def',
+        '4':'ghi',
+        '5':'jkl',
+        '6':'mno',
+        '7':'pqrs',
+        '8':'tuv',
+        '9':'wxyz'
+    }
+
+    let n=input_digit.length
+    let arr=[];
+
+    if(!n) return arr;
+
+    const bfs=(pos, str)=>{
+        if(pos===n){
+            arr.push(str)
+        }
+        else{
+            let letter=phone[num[pos]]
+
+            for(let i=0; i<letter.length; i++){
+                bfs(pos+1, str+letter[i] )
+            }
+        }
+    }
+
+    bfs(0,"")
+    return arr;
 }
 
 module.exports = letterCombinations;
